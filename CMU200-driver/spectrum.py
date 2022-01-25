ed7b4779f4d6fc3fbf6500ef7362ff44625670d6
Decee1 <decee.server@gmail.com>
2022-01-25 22:07:17 +0100

    Added a few more commands, spectrum_marker_peak is for debuggingis for debugging

--- CMU200-driver/spectrum.py
+++ CMU200-driver/spectrum.py
@@ -2,9 +2,19 @@ from communication import Communication
 
 
 class Spectrum(Communication):
-    """ Class for CMU200 spectrum configurations"""
+    """ Class for CMU200 spectrum commands"""
     def __init__(self, comport, timeout, sec_addr_serial):
         super(Spectrum, self).__init__(comport, timeout, sec_addr_serial)
+        # Configure the secoundary address
+        self.secoundary_addr = 1
+        self.write("SYSTem:REMote:ADDRess:SECondary 1 RF_NSig")
+
+    def initialize_spectrum(self):
+        command = "INITiate:SPECtrum"
+        self.write(command)
+    def abort_spectrum(self):
+        command = "ABORt:SPECtrum"
+        self.write(command)
 
     def start_freq(self, value):
         """
@@ -56,3 +66,11 @@ class Spectrum(Communication):
 
         # Todo add more functions
         # list: https://github.com/bryan-rozier/Brython/blob/master/CRTU_Spectrum.py
+
+    def spectrum_marker_peak(self):
+        """
+        Query the peak value in dB
+        """
+        return self.query("FETCh:SPECtrum:MARKer:PEAK?")
+
+
